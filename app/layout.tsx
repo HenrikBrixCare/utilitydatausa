120f4e06d66154ffa578844bdea2d32c212055cd
Henrik Brix Vestergaard <henrik@brixcare.dk>
2026-08-27 23:10:27 +0200

    Load refined UtilityDataUSA emblem styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./typography.css";
+import "./logo.css";
 import WebMCPTools from "./components/WebMCPTools";
 
 export const metadata: Metadata = {
